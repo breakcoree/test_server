03ec80fe0a998cfc8ddf619663d93fe90d864d5c
breakcoree <breakcoreb@gmail.com>
2025-10-23 10:40:16 +0500

    Update main.js

--- main.js
+++ main.js
@@ -1,47 +1,46 @@
-function print(a) {
-    console.log(a)
-}
-
-
 const http = require('http');
 const path = require('path');
 const fs = require('fs');
-console.log(fs)
 
+const basePath = 'C:\\Users\\kozyk_vs\\Desktop\\test_server';
 
 const server = http.createServer((req, res) => {
-    let file_path
-    let ext_name = path.extname(file_path)
-    let content_type
-    switch(ext_name) {
-        case '.js':
-            content_type = 'text/javascript'
-        case '.css':
-            content_type = 'text/css'
+  let filePath = path.join(basePath, req.url === '/' ? 'index.html' : req.url);
+  const ext = path.extname(filePath).toLowerCase();
+
+  const mimeTypes = {
+    '.html': 'text/html',
+    '.js': 'text/javascript',
+    '.css': 'text/css',
+    '.json': 'application/json',
+    '.png': 'image/png',
+    '.jpg': 'image/jpeg',
+    '.jpeg': 'image/jpeg',
+    '.ico': 'image/x-icon',
+  };
+
+  const contentType = mimeTypes[ext] || 'application/octet-stream';
+    
+  fs.readFile(filePath, (err, data) => {
+    if (err) {
+      console.error(err);
+
+      if (err.code === 'ENOENT') {
+        res.writeHead(404, { 'Content-Type': 'text/plain' });
+        res.end('404 Not Found');
+      } else {
+          
+        res.writeHead(500, { 'Content-Type': 'text/plain' });
+        res.end('500 Internal Server Error');
+      }
+      return;
     }
 
-    switch(req.url) {
-        case '' || '/':
-            file_path = path.join('C:\\Users\\kozyk_vs\\Desktop\\test_server', 'index.html')
-            break;
-        default:
-            file_path = path.join('C:\\Users\\kozyk_vs\\Desktop\\test_server', 'index.html')
-            break;
-    }
-
-        fs.readFile(file_path, (err, data) => {
-        if (err) {
-            print(err)
-            res.writeHead(404, {'Content-Type': 'text/plain'})
-            res.end('404asdasd')
-            return;
-        }
-
-        res.writeHead(200, {'Content-Type': 'text/html'});
-        res.end(data)
-    });
+    res.writeHead(200, { 'Content-Type': contentType });
+    res.end(data);
+  });
 });
 
 server.listen(3000, () => {
-    console.log('Server running on port 3000');
-}); 
+  console.log('Server running at http://localhost:3000');
+});
